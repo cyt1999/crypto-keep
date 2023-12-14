e79dc419eb7e0bf19ba26f7be3d1efde6adebeef
chenyutong <xb990815@gmail.com>
2023-12-14 10:12:42 +0800

    update

--- README.md
+++ README.md
@@ -9,9 +9,31 @@ near contract deploy crypto-keep.testnet use-file ./target/wasm32-unknown-unknow
 
 
 添加密码
-near contract call-function as-transaction crypto-keep.testnet add_password json-args  '{"account_id":"chenyutong.testnet","encrypted_password":"dsfasdfkiewr2341","website":"https://www.baidu.com","note":"baidu chenyutong","tags":["a","b"]}'  prepaid-gas '100.000 TeraGas' attached-deposit '0 NEAR' sign-as chenyutong.testnet network-config testnet sign-with-keychain send
+near contract call-function as-transaction crypto-keep.testnet add_password json-args  '{"account_id":"chenyutong.testnet","encrypted_password_info":"QtIgl58sVzmXaOp3iqQr7lc6UJEERd3o5p7we6jRs7pvIAtaT8S5W/7AZJE7YYb9w30nPakpIB4+RPyDVA6olJHdTSR2LtisT562d1AaiNJIS7xN6aXahWv4L2EavAFpdc1QSZIOBZs9DEmrjArtUzSHVi93/X0VtJ1IF3iBvw=="}'  prepaid-gas '100.000 TeraGas' attached-deposit '0 NEAR' sign-as chenyutong.testnet network-config testnet sign-with-keychain send
 
-near contract call-function as-transaction crypto-keep.testnet add_password json-args  '{"account_id":"xiamu.testnet","encrypted_password":"dsfasdfkiewr2341","website":"https://www.baidu.com","note":"baidu chenyutong","tags":["a","b"]}'  prepaid-gas '100.000 TeraGas' attached-deposit '0 NEAR' sign-as chenyutong.testnet network-config testnet sign-with-keychain send 
+删除密码
+near contract call-function as-transaction crypto-keep.testnet delete_password json-args  '{"account_id":"chenyutong.testnet","index":0}'  prepaid-gas '100.000 TeraGas' attached-deposit '0 NEAR' sign-as chenyutong.testnet network-config testnet sign-with-keychain send
 
 查询密码
-near contract call-function as-read-only crypto-keep.testnet get_password  json-args '{"account_id":"chenyutong.testnet"}'  network-config testnet now
+near contract call-function as-read-only crypto-keep.testnet get_passwords  json-args '{"account_id":"chenyutong.testnet"}'  network-config testnet  now
+
+
+----
+用户自己创建自己的合约，工厂合约
+
+对每一条数据，整体进行加密，然后进行存储。
+
+查询的时候，进行解密，显示结果。
+
+
+数据结构修改如下所示
+pub struct PasswordInfo {
+    name: String,
+    encrypted_password: String,
+    website: String,
+    note: String,
+}
+
+需要实现对PasswordInfo加密和解密。当使用add_password 添加密码的时候，进行加密然后存储到链上。当使用 get_password 获取密码的时候，进行解密展示出来。
+
+要使每个用户可以存储多个密码，并且能够对每个密码进行单独的管理（添加、更新、获取和删除）
--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,12 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::{env, near_bindgen};
 use near_sdk::collections::LookupMap;
-use serde::{Deserialize, Serialize};
 
 // 合约结构和初始化
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct CryptoKeep {
-    passwords: LookupMap<String, PasswordInfo>, // 用于存储密码的映射
-}
-
-#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize)]
-pub struct PasswordInfo {
-    encrypted_password: String,
-    website: String,
-    note: String,
-    tags: Vec<String>,
-}
-
-impl PasswordInfo {
-    pub fn new(encrypted_password: String, website: String, note: String, tags: Vec<String>) -> Self {
-        PasswordInfo {
-            encrypted_password,
-            website,
-            note,
-            tags,
-        }
-    }
+    passwords: LookupMap<String, Vec<String>>, // 映射到字符串的向量
 }
 
 impl Default for CryptoKeep {
@@ -41,46 +21,54 @@ impl Default for CryptoKeep {
 #[near_bindgen]
 impl CryptoKeep {
 
-    // #[init]
-    // pub fn init() -> Self {
-    //     Self {
-    //         passwords: LookupMap::new(b"p"),
-    //     }
-    // }
-
     // 创建密码
-    pub fn add_password(&mut self, account_id: String, encrypted_password: String, website: String, note: String, tags: Vec<String>) {
-        let password_info = PasswordInfo::new(encrypted_password, website, note, tags);
-        assert!(
-            self.passwords.get(&account_id).is_none(),
-            "An entry already exists for this account."
-        );
-        self.passwords.insert(&account_id, &password_info);
+    // pub fn add_password(&mut self, account_id: String, encrypted_password: String, website: String, note: String, tags: Vec<String>) {
+    //     let password_info = PasswordInfo::new(encrypted_password, website, note, tags);
+    //     assert!(
+    //         self.passwords.get(&account_id).is_none(),
+    //         "An entry already exists for this account."
+    //     );
+    //     self.passwords.insert(&account_id, &password_info);
+    // }
+    // 创建密码   
+    pub fn add_password(&mut self, account_id: String, encrypted_password_info: String) {
+        let mut passwords_list = self.passwords.get(&account_id).unwrap_or_else(Vec::new);
+        passwords_list.push(encrypted_password_info);
+        self.passwords.insert(&account_id, &passwords_list);
     }
-
+    
+    
+    
     // 更新密码
-    pub fn update_password(&mut self, account_id: String, new_encrypted_password: String, new_website: String, new_note: String, new_tags: Vec<String>) {
-        match self.passwords.get(&account_id) {
-            Some(mut password_info) => {
-                password_info.encrypted_password = new_encrypted_password;
-                password_info.website = new_website;
-                password_info.note = new_note;
-                password_info.tags = new_tags;
-                self.passwords.insert(&account_id, &password_info);
-            },
-            None => env::panic_str("No password entry exists for this account."),
+    pub fn update_password(&mut self, account_id: String, index: usize, new_encrypted_password_info: String) {
+        let mut passwords_list = self.passwords.get(&account_id).unwrap_or_else(Vec::new);
+        if passwords_list.get(index).is_some() {
+            passwords_list[index] = new_encrypted_password_info;
+            self.passwords.insert(&account_id, &passwords_list);
+        } else {
+            env::panic_str("No password entry exists at the specified index for this account.");
         }
     }
+    
 
     // 获取密码
-    pub fn get_password(&self, account_id: String) -> Option<PasswordInfo> {
-        self.passwords.get(&account_id)
+    pub fn get_passwords(&self, account_id: String)  -> Vec<(usize, String)> {
+        self.passwords.get(&account_id).unwrap_or_else(Vec::new)
+        .into_iter().enumerate().collect()
     }
+    
 
     // 删除密码
-    pub fn delete_password(&mut self, account_id: String) {
-        self.passwords.remove(&account_id);
+    pub fn delete_password(&mut self, account_id: String, index: usize) {
+        let mut passwords_list = self.passwords.get(&account_id).unwrap_or_else(Vec::new);
+        if passwords_list.get(index).is_some() {
+            passwords_list.remove(index);
+            self.passwords.insert(&account_id, &passwords_list);
+        } else {
+            env::panic_str("No password entry exists at the specified index for this account.");
+        }
     }
+    
 
 }
 
